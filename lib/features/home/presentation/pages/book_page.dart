c1a1401ab30de531a2df0920236a8be0ead4944f
TilakBistaChhetri <tilakbista980@gmail.com>
2025-06-26 19:15:23 +0545

    wip

--- lib/features/home/presentation/pages/book_page.dart
+++ lib/features/home/presentation/pages/book_page.dart
@@ -8,129 +8,117 @@ import 'package:registerapp/core/themes/app_style.dart';
 import '../../domain/entities/book.dart';
 import '../bloc/book_cubit.dart';
 import '../bloc/book_state.dart';
-import '../../data/repositories/book_repository_impl.dart';
-import '../../domain/usecases/book_usecases.dart';
+
 @RoutePage()
 class BookPage extends StatelessWidget {
   const BookPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) =>
-          BookCubit(BookUseCases(BookRepositoryImpl()))..loadBooks(),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.primaryColor,
-          title: Text(
-            'Book Register App',
-            style:
-                AppStyles.text20PxBold.copyWith(color: AppColors.whiteColor),
-          ),
-          centerTitle: true,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        title: Text(
+          'Book Register App',
+          style: AppStyles.text20PxBold.copyWith(color: AppColors.whiteColor),
         ),
-        body: BlocBuilder<BookCubit, BookState>(
-          builder: (context, state) {
-            if (state is BookLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is BookLoaded) {
-              final books = state.books;
-              return ListView.builder(
-                itemCount: books.length,
-                itemBuilder: (_, i) {
-                  final book = books[i];
-                  return Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 20.w,
-                      vertical: 6.h,
-                    ),
-                    padding: EdgeInsets.all(12.r),
-                    decoration: BoxDecoration(
-                      color: AppColors.whiteColor,
-                      borderRadius: BorderRadius.circular(12.r),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.blackColor.withOpacity(0.12),
-                          blurRadius: 4.r,
-                          offset: Offset(0, 2),
+        centerTitle: true,
+      ),
+      body: BlocBuilder<BookCubit, BookState>(
+        builder: (context, state) {
+          if (state is BookLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is BookLoaded) {
+            final books = state.books;
+            return ListView.builder(
+              itemCount: books.length,
+              itemBuilder: (_, i) {
+                final book = books[i];
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 6.h),
+                  padding: EdgeInsets.all(12.r),
+                  decoration: BoxDecoration(
+                    color: AppColors.whiteColor,
+                    borderRadius: BorderRadius.circular(12.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.blackColor.withOpacity(0.12),
+                        blurRadius: 4.r,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 4.h),
+                      Text(
+                        book.name,
+                        style: AppStyles.text20PxBold.copyWith(
+                          color: AppColors.blackColor,
                         ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 4.h),
-                        Text(
-                          book.name,
-                          style: AppStyles.text20PxBold.copyWith(
-                            color: AppColors.blackColor,
+                      ),
+                      SizedBox(height: 4.h),
+                      Text(
+                        'Book ID: ${book.id}',
+                        style: AppStyles.text15PxRegular,
+                      ),
+                      SizedBox(height: 3.h),
+                      Text(
+                        'Author Name: ${book.author}',
+                        style: AppStyles.text15PxRegular,
+                      ),
+                      SizedBox(height: 3.h),
+                      Text(
+                        'Price: ${book.price}',
+                        style: AppStyles.text15PxRegular,
+                      ),
+                      SizedBox(height: 3.h),
+                      Text(
+                        'Publication Date: ${book.publicationDate}',
+                        style: AppStyles.text15PxRegular,
+                      ),
+                      SizedBox(height: 8.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () => _showForm(context, book),
                           ),
-                        ),
-                        SizedBox(height: 4.h),
-                        Text(
-                          'Book ID: ${book.id}',
-                          style: AppStyles.text15PxRegular,
-                        ),
-                        SizedBox(height: 3.h),
-                        Text(
-                          'Author Name: ${book.author}',
-                          style: AppStyles.text15PxRegular,
-                        ),
-                        SizedBox(height: 3.h),
-                        Text(
-                          'Price: ${book.price}',
-                          style: AppStyles.text15PxRegular,
-                        ),
-                        SizedBox(height: 3.h),
-                        Text(
-                          'Publication Date: ${book.publicationDate}',
-                          style: AppStyles.text15PxRegular,
-                        ),
-                        SizedBox(height: 8.h),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.edit),
-                              onPressed: () => _showForm(context, book),
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete),
-                              onPressed: () =>
-                                  context.read<BookCubit>().deleteBook(
-                                        book.id,
-                                      ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            } else if (state is BookError) {
-              return Center(child: Text(state.message));
-            }
-            return const SizedBox.shrink();
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _showForm(context),
-          backgroundColor: AppColors.primaryColor,
-          child: const Icon(Icons.add, color: AppColors.whiteColor),
-        ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () =>
+                                context.read<BookCubit>().deleteBook(book.id),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (state is BookError) {
+            return Center(child: Text(state.message));
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(context),
+        backgroundColor: AppColors.primaryColor,
+        child: const Icon(Icons.add, color: AppColors.whiteColor),
       ),
     );
   }
+
   void _showForm(BuildContext context, [Book? book]) {
     final oldId = book?.id;
     final idController = TextEditingController(text: book?.id ?? '');
     final nameController = TextEditingController(text: book?.name);
     final authorController = TextEditingController(text: book?.author);
     final priceController = TextEditingController(text: book?.price);
-
- 
     String selectedDateStr = book?.publicationDate ?? '';
-
     final dateController = TextEditingController(text: selectedDateStr);
 
     Future<void> _pickDate() async {
@@ -139,9 +127,7 @@ class BookPage extends StatelessWidget {
         if (selectedDateStr.isNotEmpty) {
           initialDate = DateFormat('yyyy-MM-dd').parse(selectedDateStr);
         }
-      } catch (_) {
-       
-      }
+      } catch (_) {}
       final pickedDate = await showDatePicker(
         context: context,
         initialDate: initialDate,
@@ -216,7 +202,10 @@ class BookPage extends StatelessWidget {
                       newBook,
                     );
               }
-              Navigator.pop(context);
+
+              Future.delayed(Duration(milliseconds: 200), () {
+                Navigator.pop(context);
+              });
             },
             child: Text('Save', style: AppStyles.text15PxRegular),
           ),
